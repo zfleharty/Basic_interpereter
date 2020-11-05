c98f1887395a98992c5a4a601ade297b5dfb701e
zfleharty <zfleharty@unm.edu>
2020-11-04 22:11:16 -0700

    Changed symbol table to be represented as an array

--- src/basic_parse.hs
+++ src/basic_parse.hs
@@ -44,23 +44,70 @@ import Data.Array()
 import Data.IORef
 import Control.Monad
 
-data Statement      = LET Var Expression
+-- ================================== --
+-- experimenting by Hoss              --
+import qualified Data.Text    as Text
+import qualified Data.Text.IO as Text
+-- =================================== --
+
+data Statement      = FOR Var Expression Expression
+                    | FORSTEP Var Expression Expression Expression
+                    | IF Expression Constant
+                    | INPUT Var
+                    | LET Var Expression
+                    | NEXT Var
                     | PRINT Expression
                     | END 
 
+
+
+
+-- class Interpreter a b where
+--   eval :: a -> b
+
+--class EvNumExpr a where
+--   eval :: a -> Int
+
+-- instance EvNumExpr Expression Int where
+--   eval (AddExp e1 e2) = (eval e1) + (eval e2)
+--   eval (MultExp e1 e2) = (eval e1) * (eval e2)
+--   eval (ConstExp e) = num e
+--   eval (Varabile v) = do {ref <- get_Ref (character v); value <- readIORef ref; return }
+
+
+
 data Expression     = AddExp Expression Expression 
                     | MultExp Expression Expression
-                    | Cons Constant
-                    | Variable Var
+--                    | EqualsExp Expression Expression
+                    | ConstExp {cons::Constant}
+                    | Variable {var::Var}
+
+data CompareExpr    = EqualsExpr Expression Expression
+
 
 data Var            = Var {character::Char}
 
-data Constant       = Number Int | Symbol String
+data Constant       = NumConst {num::Int}
+                    | StringConst {str::String}
+
+data NegateExp      = Neg PowerExp
+                    | Pexp PowerExp -- fix this!
+
+data PowerExp      = Pow Value PowerExp
+                    | PowValue Value
+
+data Value          = ValueParens Expression
+                    | ValueVar Var
+                    | ValueFxn Function
+                    | ValueConst Constant
+
+data Function       = INT Expression
+                    | RND Expression
 
 data Line_statement = Unparsed_line {line_num:: Int, unparsed:: String} 
                     | Parsed_line {line_num:: Int, sttment:: Statement} 
 
-
+data Interpereter = Program {s_table :: Array Char (IO (IORef Constant)), program_counter:: Int}
 -------------------------------------------------------------------------------
 -- state Monad implementation. May change later can not load in State Monad  --
 -------------------------------------------------------------------------------
@@ -86,18 +133,19 @@ put s         = State $ \_ -> ((), s)
 evalState x s = fst $ runState x s
 
 
-----------------------------------------------------------------------------------------
-------------------------------Derived instances for Data types -------------------------
-----------------------------------------------------------------------------------------
+-------------------------------------------------------------
+-- Derived instances for Data types                        --
+-------------------------------------------------------------
 
 instance Show Expression where
   show (AddExp e1 e2)  = (show e1) ++ " + " ++ (show e2)
   show (MultExp e1 e2) = (show e1) ++ " * " ++ (show e2)
-  show (Cons x)        = show x
+--  show (EqualsExp e1 e2)  = (show e1) ++ " = " ++ (show e2)
+  show (ConstExp x)       = show x
   show (Variable x)    = show x
 
 instance Show Var where
-  show (Var x) = show x
+  show (Var x) = show (NoQuotesChar x)
 
 instance Eq Line_statement where
   a == b = (line_num a) == (line_num b)
@@ -108,126 +156,243 @@ instance Ord Line_statement where
 instance Show Line_statement where
   show x = "(" ++ show (line_num x) ++ ", " ++ unparsed x ++ ")" 
 
-instance Show Statement where                              
-  show (LET x y) = "LET " ++ (show x) ++ " = " ++ (show y)
-  show (PRINT e) = "PRINT " ++ show e
-  show (END)     = "END"                                   
+instance Show Statement where   
+  show (FOR x e1 e2) =
+    "FOR " ++ (show x) ++ " = " ++ (show e1) ++ " TO " ++ (show e2)
+  show (FORSTEP x e1 e2 e3) =
+    "FOR " ++ (show x) ++ " = " ++ (show e1)
+    ++ " TO " ++ (show e2) ++ " STEP " ++ (show e3)
+  show (IF e x)   = "IF " ++ (show e) ++ " THEN " ++ (show x)
+  show (INPUT x)  = "INPUT " ++ (show x)
+  show (LET x y)  = "LET " ++ (show x) ++ " = " ++ (show y)
+  show (NEXT x)   = "NEXT " ++ (show x)
+  show (PRINT e)  = "PRINT " ++ show e
+  show (END)      = "END"                                   
 
 instance Show Constant where
-  show (Number x) = show x
-  show (Symbol x) = show (NoQuotes x)
+  show (NumConst x) = show x
+  show (StringConst x) = show (NoQuotes x)
+
+instance Show Value where
+  show (ValueVar x)   = show x
+  -- show (ValueFxn x)   = show x
+  -- show (ValueConst x) = show x
+
+instance Show Function where
+  show (INT e)    = "INT(" ++ (show e) ++ ")"
+  show (RND e)    = "RND(" ++ (show e) ++ ")"
 
 newtype NoQuotes = NoQuotes String
+newtype NoQuotesChar = NoQuotesChar Char
 
 instance Show NoQuotes where show (NoQuotes str) = str
+instance Show NoQuotesChar where show (NoQuotesChar char) = show (NoQuotes [char])
 
------------------------------_Main_----------------------------------------------
+-----------------------------_Main_-------------------------------------
 
-tuple_line         :: Parser Int
-tuple_line         = do {num <- int; return num}
+tuple_line :: Parser Int
+tuple_line = do {num <- int; return num}
 
-tupled_lines       :: [String] -> [Line_statement]
-tupled_lines ls    = [let l_statement = Unparsed_line (fst tuple) (snd tuple)
+-- tupled_lines     Processes a list of strings, where each string
+--                  represents a line from a BASIC program, producing a
+--                  list of tuple-like Unparsed_Line constructions
+--                  which print out like (line #, statement)
+tupled_lines :: [String] -> [Line_statement]
+tupled_lines ls = [let l_statement = Unparsed_line (fst tuple) (snd tuple)
                             where tuple = head (parse tuple_line x)
                                     in l_statement | x <- ls]
 
+-- parse_lines      Processes a list of Line_Statements, typically
+--                  produced by the tupled_lines fxn and which appear
+--                  as tuples of the form (line #, Line_statement),
+--                  returning a list of tuples of the form
+--                  (new line #, Statement) where the new line #s now
+--                  run consecutively 1, 2, 3, etc.
+parse_lines :: (Num a, Enum a) => [Line_statement] -> [(a, Statement)]
 parse_lines lines = [let p_line = (n , (fst . head) stment)
                                where stment = parse statement (unparsed ls)
                                in p_line | (n,ls) <- zip [1..] (sort lines)]
 
----------------------------------------------------------------------------------------
--- Change to search for GoToStatements and renumber them according to the reordering --
----------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------
+-- Change to search for GoToStatements and renumber them according to --
+-- the reordering                                                     --
+------------------------------------------------------------------------
 
 
 --------------NEXT STEP------
 --Attempting to implement a threaded symbol table somehow
 --First trying to have a threaded symbol table of type list and eventually switch it to any Array of IORefs
---symbol_table = [(i,(Number 0)) | i <- ['A' ..'Z']]
-
--- writeToTable' var val = do tab <- get
---                           let newTab = [if (character var) == s then (s,val) else (s,v) | (s,v) <- tab]
---                           put newTab
---                           return tab
-
--- readFromTable' var = do tab <- get
---                        let varvar = (head) [v | (s,v) <- tab, s == (character var)]
---                        return (varvar)
 
 
 
+symbol_table = [(i,(NumConst 0)) | i <- ['A' ..'Z']]
+
+edit_table :: [(Char,Constant)] -> Char -> Constant -> [(Char,Constant)]
+edit_table [] _ _ = []
+edit_table ((s,v):rest) sym val = if sym == s
+  then (s,val) : rest
+  else (s,v) : (edit_table rest sym val)
 
 
-writeToTable var val = do tab <- get
-                          let ref = (tab ! var)
-                          return $ writeIORef ref val
-                              --return ()
+
+read_table ((_,v):[]) _ = v
+read_table ((s,v):rest) sym = if sym == s then v else read_table rest sym
+
+write_to_table var val = do tab <- get
+                            put (edit_table tab var val)
+                            return ()
+
+read_from_table var = do tab <- get
+                         return (read_table tab var)
 
 
-readFromTable tab var = do ref <- (tab ! var)
-                           return (readIORef ref)
+test = (evalState $ eval_test 'A' (NumConst 43)) symbol_table
+eval_test var val = do {write_to_table 'A' val; nv <- read_from_table var; return nv}
+-------------------------------------------------------------------------------------------------------------
+-- writeToTable var val = do tab <- get                                                                    --
+--                           let newTab = [if (character var) == s then (s,val) else (s,v) | (s,v) <- tab] --
+--                           put tab                                                                       --
+--                           return tab                                                                    --
+--                                                                                                         --
+-- readFromTable' var = do tab <- get                                                                      --
+--                        let varvar = (head) [v | (s,v) <- tab, s == (character var)]                     --
+--                        return (varvar)                                                                  --
+--                                                                                                         --
+-- readRefFromTable :: Char -> State Interpereter (IO (IORef Constant))                                    --
+-- readRefFromTable var = do prgrm_info <- get                                                             --
+--                           return ((s_table prgrm_info) ! var)                                           --
+--                                                                                                         --
+-- get_ref :: Char -> Interpereter -> IO (IORef Constant)                                                  --
+-- get_ref var = (evalState $ readRefFromTable var)                                                        --
+-------------------------------------------------------------------------------------------------------------
 
 
-runtest = (evalState $ test 'A') symbol_table
-test var = do {tab <- get; writeToTable tab (var) (Number 3); variable <- readFromTable tab (var); return variable}
-          
+
 
 main = do
-  args <- getArgs
-  fileExists <- doesFileExist $ head args
-  if fileExists 
-    then do handle <- openFile (head args) ReadMode
+  --args <- getArgs 
+  --fileExists <- doesFileExist $ head args
+  if True -------------------- fileExists __________________Changed for testing inside interactive GHCI without command line args
+    then do handle <- openFile ("foo.bas") ReadMode
             content <- hGetContents handle
             let sorted_array = array bound sorted_lines
                   where sorted_lines = parse_lines $ tupled_lines (lines content)
                         bound = (1, length sorted_lines)
-                       
-    
+            let symbol_table = (array ('A','Z') [ (i,newIORef (NumConst 0))| i <- ['A'..'Z']])
+            let prgrm = Program symbol_table 1
             putStrLn $ show sorted_array
             
-    else do
-    let symbol_table = array ('A','Z') [ (i,newIORef (Number 0))| i <- ['A'..'Z']]
-    
-    putStrLn $ "File " ++ (head args) ++ " Does Not Exist."
+    else do putStrLn $ "File " ++ ("") ++ " Does Not Exist."
+
+-- ================================== --
+-- experimenting by Hoss              --
+main2 = do
+    ls <- fmap Text.lines (Text.readFile "foo.bas")
+    do 
+      let ls' = map Text.unpack ls
+      putStrLn $ show ls'
+-- =================================== --
+
+
+
   
 -- =========================================== --
 --  Parsers for special individual characters  --
 -- =========================================== --
+
 equal = char ('=')
 left  = char ('(')
 right = char (')')
+
 -- =========================================== --
 --  Parsers for special key strings            --
 -- =========================================== --
+
 p_end :: Parser String
 p_end = string "END"
+
+p_for :: Parser String
+p_for = string "FOR"
+
+p_if :: Parser String
+p_if = string "IF"
+
+p_input :: Parser String
+p_input = string "INPUT"
 
 p_let :: Parser String
 p_let = string "LET"
 
+p_next :: Parser String
+p_next = string "NEXT"
+
 p_print :: Parser String
 p_print = string "PRINT"
+
+p_then :: Parser String
+p_then = string "THEN"
+
+p_to :: Parser String
+p_to = string "TO"
+
 -- =========================================== --
 --  Parsers for particular Statements          --
 -- =========================================== --
 
 statement       :: Parser Statement
-let_statement   :: Parser Statement
 end_statement   :: Parser Statement
+-- for_statement   :: Parser Statement
+--if_statement    :: Parser Statement
+input_statement :: Parser Statement
+let_statement   :: Parser Statement
+-- next_statement  :: Parser Statement
 print_statement :: Parser Statement
 
+statement =
+  for_statement +++ input_statement +++ --if_statement
+  let_statement +++ next_statement +++ print_statement +++
+  end_statement
+  
 
+end_statement = do {_ <- token p_end; return END}
 
-statement     = let_statement +++ end_statement +++ print_statement
+-- if_statement needs work: getting confused here with expressions
+-- would be helpful to have a separate Boolean expression type
+-- but our BASIC grammar doesn't seem to have that
+-- Temporarily looking only for EqualsExp in the IF
+-- if_statement = do
+--   token p_if
+--   boolExpr <- token equals_expr
+--   token p_then
+--   c <- token p_const
+--   return (IF boolExpr c)
+
+input_statement = do
+  token p_input
+  var <- token p_var
+  return (INPUT var)
+
+for_statement = do
+  token p_for
+  var <- token p_var
+  token equal
+  fromExpr <- token expr
+  token p_to
+  toExpr <- token expr
+  return (FOR var fromExpr toExpr)
 
 let_statement = do
   _ <- token p_let
   var <- token p_var
   _ <- token equal
-  val <- token p_cons
-  return (LET var (Cons val))
-  
-end_statement   = do {a <- token p_end; return END}
+  val <- token p_const
+  return (LET var (ConstExp val))
+
+next_statement = do
+  token p_next
+  var <- token p_var
+  return (NEXT var)
 
 print_statement = do {token p_print; e <- expr; return (PRINT e)}
 
@@ -236,7 +401,7 @@ print_statement = do {token p_print; e <- expr; return (PRINT e)}
 --  Statement components                       --
 -- =========================================== --
 
-p_cons   :: Parser Constant
+p_const  :: Parser Constant
 p_number :: Parser Constant
 p_var    :: Parser Var
 p_symbol :: Parser Constant
@@ -251,35 +416,50 @@ var_char_end x = elem x "$%"
 
 
   
-p_cons   = p_number +++ p_symbol
+p_const   = p_number +++ p_symbol
 
-p_number = do {d <- token int; return (Number d)}
+p_number = do {d <- token int; return (NumConst d)}
 
 p_var    = do {var <- token upper; return (Var var)}
 
 p_symbol = do {a <- sat (isAlpha); b <- many (sat var_char);
-               c <- many (sat var_char_end); return (Symbol (a:(b++c)))}
+               c <- many (sat var_char_end); return (StringConst (a:(b++c)))}
 
 
-var_expr :: Parser Expression
-num_expr :: Parser Expression
-expr     :: Parser Expression
-value    :: Parser Expression
-add_exp  :: Parser Expression
-mult_exp :: Parser Expression
-
-
+var_expr    :: Parser Expression
+num_expr    :: Parser Expression
+expr        :: Parser Expression
+add_exp     :: Parser Expression
+mult_exp    :: Parser Expression
+--equals_expr :: Parser Expression
+value       :: Parser Expression
 
 
 var_expr = do {var <- token upper; return (Variable (Var var))}
 
-num_expr = do {d <- token int; return (Cons (Number d))}
+num_expr = do {d <- token int; return (ConstExp (NumConst d))}
 
 expr     = add_exp +++ mult_exp
 
-add_exp  = do {x <- token mult_exp; token (char '+');y <- token add_exp; return (AddExp x y)} +++ mult_exp
+add_exp  = do {
+  x <- token mult_exp;
+  token (char '+');
+  y <- token add_exp;
+  return (AddExp x y)} +++ mult_exp
 
-mult_exp = do {x <- token value; token (char '*'); y <- token mult_exp; return (MultExp x y)} +++ value
+mult_exp = do {
+  x <- token value;
+  token (char '*');
+  y <- token mult_exp;
+  return (MultExp x y)} +++ value
+
+-- set up for Expression = Expression
+-- may be too general for our needs and rely on inadeq expr parser
+-- equals_expr = do
+--   x <- token expr
+--   token equal
+--   y <- token expr
+--   return (EqualsExp x y)
 
 value    = do
   many (char '(')
@@ -288,7 +468,34 @@ value    = do
   return expr
 
 
+-- ============================== --
+--  some definitions for testing  --
+-- ============================== --
 
+test_number_1 = ConstExp (NumConst 1)
+test_number_5 = ConstExp (NumConst 5)
+test_number_10 = ConstExp (NumConst 10)
+test_conststring = StringConst "ABC"
+test_var_x = Var 'X'
+test_var_y = Var 'Y'
+test_valuevar = ValueVar test_var_x
+-- test_valuefxn = ValueFxn (RND (Variable test_var_x))
+-- test_valueconst = ValueConst (NumConst 10)
+--test_expr_equals = EqualsExp (Variable test_var_x) (ConstExp (NumConst 10))
+test_statement_for = FOR test_var_x test_number_5 test_number_10
+test_statement_forstep =
+  FORSTEP test_var_x test_number_5 test_number_10 test_number_1
+--test_statement_if = IF test_expr_equals (NumConst 100)
+test_statement_input = INPUT test_var_y
+test_statement_let = LET test_var_x test_number_5
+test_statement_next = NEXT test_var_x
+test_program = "10 LET A = 2\n20 LET B = 3\n30 LET C = 4\n40 PRINT A * (B + C)\n50 END"
+test_program_fail = "10 LET A = 2\n20 LET B = 3\nLET C = 4\n40 PRINT A * (B + C)\n50 END"
+test_program_list = ["10 LET A = 2",
+                     "20 LET B = 3",
+                     "30 LET C = 4",
+                     "40 PRINT A * (B + C)",
+                     "50 END"]
 
   
 
