a5232aa8249a1fc0d0be86c187d53d71789c8279
Warren Craft <wdcraft@unm.edu>
2020-11-14 17:51:35 -0700

    Semi-complete data types for the BASIC grammar
    Semi-complete in the sense that this has not been tested and is still out-of-sync with the companion code in basic_parse_augmented.hs. It does include most if not all the relevant Show instance declarations as well.

--- src/BasicTypesAugmented.hs
+++ src/BasicTypesAugmented.hs
@@ -1,39 +1,68 @@
-module BasicTypes
+module BasicTypesAugmented
   (Statement(..),
+   IDList(..),
+   ArrayList(..),
+   IntegerList(..),
+   ExpressionList(..),
+   PrintList(..),
    Expression(..),
+   AndExpr(..),
+   NotExpr(..),
    CompareExpr(..),
-   Constant(..),
+   AddExpr(..),
+   MultExpr(..),
    NegateExpr(..),
    PowerExpr(..),
    Value(..),
-   Function(..),
    Var(..),
+   Array(..),
+   Function(..),
+   Constant(..),
+   ID(..),
    Line_statement(..),
    Interpreter(..)) where
 
 {-# LANGUAGE MultiParamTypeClasses #-}
-data Statement      = FOR Var Expression Expression
-                    | FORSTEP Var Expression Expression Expression
-                    | IF CompareExpr Constant
-                    | INPUT Var
-                    | LET Var Expression
-                    | NEXT Var
-                    | PRINT Expression
+
+data Statement      = DIM ArrayList
                     | END
+                    | FOR ID Expression Expression
+                    | FORSTEP Var Expression Expression Expression
+                    | GOTO Int
+                    | GOSUB Int
+                    | IF Expression Int
+                    | INPUT String IDList
+                    | LET Var Expression
+                    | NEXT IDList
+                    | ON Expression IntegerList
+                    | PRINT PrintList
+                    | PRINT_TAB Value PrintList
+                    | REM String
+                    | RETURN
+                    | EQUALS Var Expression
 
-data Expression     = AddExpr Expression Expression
-                    | MultExpr Expression Expression
-                    | ConstExpr {const::Constant}
-                    | Variable {var::Var}
-                    | FxnExpr Function
+data IDList         = IDList [ID]
 
-data AndExpr        = AndExpr NotExpr AndExp
-                    | AndNotExpr NotExpr
+data ArrayList      = ArrayList [Array]
 
-data NotExpr        = NotExpr CompareExpr
-                    | NotNotExpr CompareExpr
+data IntegerList    = IntegerList [Int]
 
-data CompareExpr    = EqualsExpr Expression Expression
+data ExpressionList = ExpressionList [Expression]
+
+data PrintList      = PrintList Expression
+                    | PrintListComma [Expression]      -- tab sep
+                    | PrintListSemiColon [Expression]  -- space sep 
+
+data Expression     = OrExpr AndExpr Expression
+                    | Expr AndExpr
+
+data AndExpr        = AndExpr NotExpr AndExpr
+                    | NotAndExpr NotExpr
+
+data NotExpr        = NotCompExpr CompareExpr
+                    | NotExpr CompareExpr
+
+data CompareExpr    = CompEqualsExpr AddExpr CompareExpr
                     | CompNotEqualExpr AddExpr CompareExpr
                     | CompGreaterExpr AddExpr CompareExpr
                     | CompGreaterEqExpr AddExpr CompareExpr
@@ -41,26 +70,36 @@ data CompareExpr    = EqualsExpr Expression Expression
                     | CompLessEqExpr AddExpr CompareExpr
                     | CompExpr AddExpr
 
-data AddExpr        = AddExpr
+data AddExpr        = AddExpr MultExpr AddExpr
+                    | SubAddExpr MultExpr AddExpr
+                    | MultAddExpr MultExpr
 
-data Var            = Var {character::Char}
+data MultExpr       = MultExpr NegateExpr MultExpr
+                    | DivMultExpr NegateExpr MultExpr
+                    | NegMultExpr NegateExpr
+
+data NegateExpr     = NegExpr PowerExpr
+                    | PowNegExpr PowerExpr
+
+data PowerExpr      = PowExpr Value PowerExpr
+                    | ValPowExpr Value
+
+data Value          = ParensVal Expression
+                    | VarVal Var
+                    | FxnVal Function
+                    | ConstVal Constant
+
+data Var            = IDVar ID
+
+data Array          = IDArray ID [Expression]  -- ??
+
+data Function       = INT Expression
+                    | RND Expression
 
 data Constant       = NumConst {num::Int}
                     | StringConst {str::String}
 
-data NegateExpr     = Neg PowerExpr
-                    | Pexpr PowerExpr -- fix this!
-
-data PowerExpr      = Pow Value PowerExpr
-                    | PowValue Value
-
-data Value          = ValueParens Expression
-                    | ValueVar Var
-                    | ValueFxn Function
-                    | ValueConst Constant
-
-data Function       = INT Expression
-                    | RND Expression
+data ID             = ID {character::Char}
 
 data Line_statement = Unparsed_line {line_num:: Int, unparsed:: String}
                     | Parsed_line {ix:: Int, origLine:: Int, sttment:: Statement}
@@ -71,21 +110,83 @@ data Interpreter = Program {s_table :: [(Char,Constant)], program_counter:: Int}
 -- Derived instances for Data types                        --
 -------------------------------------------------------------
 
+instance Show IDList where
+  show (IDList []) = "( )"
+  show (IDList (id:ids)) = "(" ++ (show id) ++ (showCdr ids) ++ ")"
+
+instance Show PrintList where
+  show (PrintList e) = show e
+  show (PrintListComma (e:es)) = (show e) ++ (showCdr es)
+  show (PrintListSemiColon (e:es)) = (show e) ++ (showCdrSemi es)
+
+showCdr [] = ""
+showCdr (x:xs) = ", " ++ (showCdr xs)
+showCdrSemi [] = ""
+showCdrSemi (x:xs) = "; " ++ (showCdr xs)
+
 instance Show Expression where
-  show (AddExpr e1 e2)  = (show e1) ++ " + " ++ (show e2)
-  show (MultExpr e1@(AddExpr e11 e12) e2@(AddExpr e21 e22)) =
-    "(" ++ (show e1) ++ ")" ++ " * " ++ "(" ++ (show e2) ++ ")"
-  show (MultExpr e1@(AddExpr e11 e12) e2) = 
-    "(" ++ (show e1) ++ ")" ++ " * " ++ (show e2)
-  show (MultExpr e1 e2@(AddExpr e21 e22)) =
-    (show e1) ++ " * " ++ "(" ++ (show e2) ++ ")"
-  show (MultExpr e1 e2) = (show e1) ++ " * " ++ (show e2)
-  show (ConstExpr x)    = show x
-  show (Variable x)     = show x
-  show (FxnExpr x)      = show x
+  show (OrExpr a e) = (show a) ++ " OR " ++ (show e)
+  show (Expr a)     = show a
+
+instance Show AndExpr where
+  show (AndExpr n a) = (show n) ++ " AND " ++ (show a)
+  show (NotAndExpr e) = show e
+
+instance Show NotExpr where
+  -- not at all clear we ever use the NotCompExpr constructor!
+  show (NotCompExpr c) = " NOT(" ++ (show c) ++ ")"
+  show (NotExpr c)     = show c
+
+instance Show CompareExpr where
+  show (CompEqualsExpr a c) = (show a) ++ " = " ++ (show c)
+  show (CompNotEqualExpr a c) = (show a) ++ " <> " ++ (show c)
+  show (CompGreaterExpr a c) = (show a) ++ " > " ++ (show c)
+  show (CompGreaterEqExpr a c) = (show a) ++ " >= " ++ (show c)
+  show (CompLessExpr a c) = (show a) ++ " < " ++ (show c)
+  show (CompLessEqExpr a c) = (show a) ++ " <= " ++ (show c)
+  show (CompExpr a) = show a
+
+instance Show AddExpr where
+  show (AddExpr m a) = (show m) ++ " + " ++ (show a)
+  show (SubAddExpr m a) = (show m) ++ " - " ++ (show a)
+  show (MultAddExpr m) = show m
+
+instance Show MultExpr where
+  show (MultExpr n m) = (show n) ++ " * " ++ (show m)
+  show (DivMultExpr n m) = (show n) ++ " / " ++ (show m)
+  show (NegMultExpr n) = show n
+
+instance Show NegateExpr where
+  show (NegExpr p) = "-" ++ (show p)
+  show (PowNegExpr p) =  show p
+
+instance Show PowerExpr where
+  show (PowExpr v p) = (show v) ++ "^" ++ (show p)
+  show (ValPowExpr v) = show v
+
+instance Show Value where
+  show (ParensVal e) = "(" ++ (show e) ++ ")"
+  show (VarVal v) = show v
+  show (FxnVal f) = show f
+  show (ConstVal c) = show c
 
 instance Show Var where
-  show (Var x) = show (NoQuotesChar x)
+  show (IDVar (ID c)) = show (NoQuotesChar c)
+
+instance Show Array where
+  show (IDArray c []) = (show c) ++ "( )"
+  show (IDArray c exprlist) = (show c) ++ "(" ++ (show exprlist) ++ ")"
+
+instance Show Function where
+  show (INT e) = "INT(" ++ (show e) ++ ")"
+  show (RND e) = "RND(" ++ (show e) ++ ")"
+
+instance Show Constant where
+  show (NumConst n) = (show n)
+  show (StringConst s) = show (NoQuotes s)
+
+instance Show ID where
+  show (ID c) = show c
 
 instance Eq Line_statement where
   a == b = (line_num a) == (line_num b)
@@ -105,28 +206,12 @@ instance Show Statement where
     "FOR " ++ (show x) ++ " = " ++ (show e1)
     ++ " TO " ++ (show e2) ++ " STEP " ++ (show e3)
   show (IF e x)  = "IF "    ++ (show e) ++ " THEN " ++ (show x)
-  show (INPUT x) = "INPUT " ++ (show x)
+  show (INPUT s idlist) = "INPUT " ++ (show s) ++ " " ++ (show idlist)
   show (LET x y) = "LET "   ++ (show x) ++ " = "    ++ (show y)
   show (NEXT x)  = "NEXT "  ++ (show x)
   show (PRINT e) = "PRINT " ++ (show e)
   show (END)     = "END"
 
-instance Show CompareExpr where
-  show (EqualsExpr a b) =  show a ++ " == " ++ show b
-
-instance Show Constant where
-  show (NumConst x) = show x
-  show (StringConst x) = show (NoQuotes x)
-
-instance Show Value where
-  show (ValueVar x)   = show x
-  show (ValueFxn x)   = show x
-  show (ValueConst x) = show x
-  show (ValueParens x) = show x
-
-instance Show Function where
-  show (INT e)    = "INT(" ++ (show e) ++ ")"
-  show (RND e)    = "RND(" ++ (show e) ++ ")"
 
 newtype NoQuotes = NoQuotes String
 newtype NoQuotesChar = NoQuotesChar Char
