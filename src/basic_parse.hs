79733cbf77b060d24790e36a8e648d240b1084e1
zfleharty <zfleharty@unm.edu>
2020-11-20 14:43:48 -0700

    Changed eval for statement and expression to know longer use State for random number generator. use RandomRIO since already in IO monad. Deleted sections of old testing code

--- src/basic_parse.hs
+++ src/basic_parse.hs
@@ -81,85 +81,83 @@ parse_lines lines = [let p_line = Parsed_line n  (line_num ls) ((fst . head) stm
 --------------------- Evaluate Expression types -----------------------------
 -----------------------------------------------------------------------------
 
-eval_expr arr e prog = (runStateT $ (runReaderT $ eval_expr2' e) arr) prog
+eval_comp_expr arr e = (runReaderT $ eval_comp_expr' e) arr
 
-eval_expr'   :: Expression -> ReaderT (IOArray Char Constant) (StateT Program IO) (Float)
-eval_expr' e = do
+eval_comp_expr' e = do
   tab <- ask
   prog <- lift get
-  let r = (\e' -> (runStateT $ (runReaderT $ eval_expr' e') tab) prog)
+  case e of
+    (CompEqualsExpr e1 e2) -> do
+      v1 <- liftIO $ (eval_expr tab e1)
+      v2 <- liftIO $ (eval_expr tab e2)
+      return $ v1 == v2
+
+
+eval_expr arr e = (runReaderT $ eval_expr' e) arr
+
+eval_expr'   :: Expression -> ReaderT (IOArray Char Constant) IO (Float)
+eval_expr' e = do
+  tab <- ask
+  let r = (\e' -> (runReaderT $ eval_expr' e') tab)
     in case e of
+
          AddExpr e1 e2 -> do
-           (i1,_) <- liftIO $ r e1
-           (i2,_) <- liftIO $ r e2
-           return $ i1 + i2
+           i1 <- liftIO $ r e1
+           i2 <- liftIO $ r e2
+           return $ i1 + i2           
+
          MultExpr e1 e2 -> do
-           (i1,_) <- liftIO $ r e1
-           (i2,_) <- liftIO $ r e2
-           return $ i1 * i2
+           i1 <- liftIO $ r e1
+           i2 <- liftIO $ r e2
+           return $ i1 * i2           
+
          ConstExpr c ->  return $ num c
+
          FxnExpr (INT e') -> do
-           (frac,_) <- liftIO $ r e'
+           frac <- liftIO $ r e'
            return ((fromIntegral .floor) frac)
+
          FxnExpr (RND e') -> do
-           (frac,_) <- liftIO $ r e'
-           let sgen = gen prog
+           frac <- liftIO $ r e'
            if frac > 1             
              then do
-             let (result,newGen) = uniformR (0, (frac - 1)) sgen
-             lift $ put (ProgInfo newGen)
-             return (fromIntegral.ceiling $ result)
+             rand <- (randomRIO (0,frac))
+             return $ (fromIntegral.floor) rand
              else do
-             let (result,newGen) = uniformR (0::Float,1::Float) sgen
-             lift $ put (ProgInfo newGen)
-             return result
+             rand <- randomRIO (0::Float,1::Float)
+             return rand
+
          VarExpr (Var v) -> do
            constValue <- liftIO $ (readArray tab v)
            return $ (num constValue)
 
 
-eval_sttmnt       :: Statement -> Program -> (IOArray Char Constant) -> IO ()
-eval_sttmnt s p arr = (runReaderT $ eval_statement2 s p) arr             
+eval_sttmnt       :: Statement -> (IOArray Char Constant) -> IO ()
+eval_sttmnt s arr = (runReaderT $ eval_statement s) arr             
 
-eval_statement    :: Statement -> Program -> ReaderT (IOArray Char Constant) IO ()
-eval_statement s p = case s of                                        
+eval_statement    :: Statement -> ReaderT (IOArray Char Constant) IO ()
+eval_statement s= case s of                                        
                       (LET (Var i) e) -> do              
                         table <- ask
-                        (c,np) <- liftIO (eval_expr2 table e p)
+                        c <- liftIO (eval_expr table e)
                         liftIO $ writeArray table i (NumConst c)                
                       (PRINT e) -> do                                
                         table <- ask
-                        (e',np) <- liftIO (eval_expr2 table e p)
+                        e' <- liftIO (eval_expr table e)
                         liftIO $ putStrLn . show $ e'
                       END -> liftIO $ exitWith ExitSuccess                
                       INPUT (Var c) -> do
                         table <- ask
                         inp <- liftIO $ readLn
                         liftIO $ writeArray table c (NumConst inp)
-              
-
-                      
+                        
+                                    
 eval_line (Parsed_line i ol s) p = do
   eval_sttmnt s p
 
 create_program_array content = array bound [(ix ls, ls) | ls <- sorted_lines]
                                where sorted_lines = parse_lines $ tupled_lines (lines content)
                                      bound = (1, length sorted_lines)
-
-
-testing file = do
-  handle <- openFile file ReadMode
-  content <- hGetContents handle
-  let sorted_array = create_program_array content
-  -- might the symbol table need to hold epxressions more generally
-  -- rather than just NumConst's?
-  symbol_table <- newArray ('A','Z') (NumConst 0) :: IO (IOArray Char Constant)
-  putStrLn $ "Entire sorted_array is: " ++ (show (assocs sorted_array))
-  let inp_test = sorted_array ! 2
-  putStrLn $ "Example item from sorted_array: " ++ show inp_test
---  ((eval_line inp_test) symbol_table)
---  readArray symbol_table ('H')
-
 
 
 main = do
@@ -177,17 +175,6 @@ main = do
             let line_map = fromList line_table
             putStrLn $ show line_map
     else do putStrLn $ "File " ++ ("") ++ " Does Not Exist."
-
--- ================================== --
--- experimenting by Hoss              --
--- main2 = do
---     ls <- fmap Text.lines (Text.readFile "foo.bas")
---     do 
---       let ls' = map Text.unpack ls
---       putStrLn $ show ls'
--- =================================== --
-
-
 
   
 
@@ -241,233 +228,3 @@ test_program_list_02 = ["30 LET C = 4",
                      "10 LET A = 2",
                      "40 PRINT A * (B + C)",
                      "50 END"]
-
-
-
-eval_expr2 arr e prog = (runStateT $ (runReaderT $ eval_expr2' e) arr) prog
-
-eval_expr2'   :: Expression -> ReaderT (IOArray Char Constant) (StateT Program IO) (Float)
-eval_expr2' e = do
-  tab <- ask
-  prog <- lift get
-  let r = (\e' -> (runStateT $ (runReaderT $ eval_expr2' e') tab) prog)
-    in case e of
-         AddExpr e1 e2 -> do
-           (i1,_) <- liftIO $ r e1
-           (i2,_) <- liftIO $ r e2
-           return $ i1 + i2
-         MultExpr e1 e2 -> do
-           (i1,_) <- liftIO $ r e1
-           (i2,_) <- liftIO $ r e2
-           return $ i1 * i2
-         ConstExpr c ->  return $ num c
-         FxnExpr (INT e') -> do
-           (frac,_) <- liftIO $ r e'
-           return ((fromIntegral .floor) frac)
-         FxnExpr (RND e') -> do
-           (frac,_) <- liftIO $ r e'
-           let sgen = gen prog
-           if frac > 1             
-             then do
-             let (result,newGen) = uniformR (0, (frac - 1)) sgen
-             lift $ put (ProgInfo newGen)
-             return (fromIntegral.ceiling $ result)
-             else do
-             let (result,newGen) = uniformR (0::Float,1::Float) sgen
-             lift $ put (ProgInfo newGen)
-             return result
-         VarExpr (Var v) -> do
-           constValue <- liftIO $ (readArray tab v)
-           return $ (num constValue)
-
-
-eval_sttmnt2       :: Statement -> Program -> (IOArray Char Constant) -> IO ()
-eval_sttmnt2 s p arr = (runReaderT $ eval_statement2 s p) arr             
-
-eval_statement2    :: Statement -> Program -> ReaderT (IOArray Char Constant) IO ()
-eval_statement2 s p = case s of                                        
-                      (LET (Var i) e) -> do              
-                        table <- ask
-                        (c,np) <- liftIO (eval_expr2 table e p)
-                        liftIO $ writeArray table i (NumConst c)                
-                      (PRINT e) -> do                                
-                        table <- ask
-                        (e',np) <- liftIO (eval_expr2 table e p)
-                        liftIO $ putStrLn . show $ e'
-                      END -> liftIO $ exitWith ExitSuccess                
-                      INPUT (Var c) -> do
-                        table <- ask
-                        inp <- liftIO $ readLn
-                        liftIO $ writeArray table c (NumConst inp)
-              
-
-
-test_eval_stmnt s arr = do
-  let p = ProgInfo (mkStdGen 10)
-  eval_sttmnt2 s p arr
-
-test_eval_expr e = do
-  arr <- test_io_array
-  let p = ProgInfo (mkStdGen 10)
-  eval_expr2 arr e p
-
-
-test_s1 = do
-  arr <- test_io_array
-  let s1 = LET test_var_y test_number_10
-  let s2 = LET test_var_x (FxnExpr test_rnd_fxn_01)
-  test_eval_stmnt s1 arr
-  test_eval_stmnt s2 arr
-  y <- readArray arr 'Y'
-  x <- readArray arr 'X'
-  putStrLn $ show (x,y) -- Expect (rand,10)
-
-test_rnd = do
-  let e = FxnExpr . RND . ConstExpr . NumConst $ 10
-  arr <- test_io_array
-  let p = ProgInfo (mkStdGen 400)
-  (v1,prog1) <- eval_expr2 arr e p 
-  (v2,prog2) <- eval_expr2 arr e prog1 
-  (v3,prog3) <- eval_expr2 arr e prog2 
-  return $ v1:v2:v3:[]
-  
-  
-  
-test_02 = do
-  let lines = tupled_lines test_program_list_02
-  putStrLn $ "lines = " ++ (show lines)
-  let sorted_lines = sort lines
-  putStrLn $ "sorted_lines = " ++ (show sorted_lines)
-  let zipped_sorted_lines = zip [1..] sorted_lines
-  putStrLn $ "zipped_sorted_lines = " ++ (show zipped_sorted_lines)
-  let head_of_zipped = head zipped_sorted_lines
-  putStrLn $ "head_of_zipped = " ++ (show head_of_zipped)
-  let (n, ls) = head_of_zipped
-  let stment = parse statement (unparsed ls)
-  putStrLn $ "stment = " ++ (show stment)
-  let str_of_stment = (fst . head) stment
-  putStrLn $ "str_of_stment = " ++ (show str_of_stment)
-  let parsed_line_construction = Parsed_line {ix=1, origLine=10, sttment = str_of_stment}
-  putStrLn $ "parsed_line_construction = " ++ (show parsed_line_construction)
-
-test_03 = do
-  let symbol_array = array ('A', 'Z') [(c, 0) | c <- ['A'..'Z']]
-  putStrLn $ show symbol_array
-  -- modify the entry for 'B' from 0 to 3:
-  arr <- (return $ symbol_array // [('B', 3)])
-  -- let symbol_array = symbol_array // [('B', 3)]
-  putStrLn $ show arr
-  -- putStrLn $ show symbol_array
-
-test_04 = do
-  -- arr <- newArray (1,10) 37 :: IO (IOArray Int Int)
-  arr <- newArray ('A','Z') 0 :: IO (IOArray Char Int)
-  -- a <- readArray arr 1
-  a <- readArray arr 'B'
-  -- writeArray arr 1 64
-  writeArray arr 'B' 1 
-  -- b <- readArray arr 1
-  b <- readArray arr 'B'
-  
-  print (a,b)
-  -- putStrLn $ show arr
-
-test_05 =  do
-  putStrLn ""
-  putStrLn $ "test_int_fxn_01: " ++ (show test_int_fxn_01)
-  let parsedExpr = parse expr (show test_int_fxn_01)
-  putStrLn $ "parse expr test_int_fxn_01: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_int_fxn_02: " ++ (show test_int_fxn_02)
-  let parsedExpr = parse expr (show test_int_fxn_02)
-  putStrLn $ "parse expr test_int_fxn_02: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_int_fxn_03: " ++ (show test_int_fxn_03)
-  let parsedExpr = parse expr (show test_int_fxn_03)
-  putStrLn $ "parse expr test_int_fxn_03: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_rnd_fxn_01: " ++ (show test_rnd_fxn_01)
-  let parsedExpr = parse expr (show test_rnd_fxn_01)
-  putStrLn $ "parse expr test_rnd_fxn_01: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_rnd_fxn_02: " ++ (show test_rnd_fxn_02)
-  let parsedExpr = parse expr (show test_rnd_fxn_02)
-  putStrLn $ "parse expr test_rnd_fxn_02: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_rnd_fxn_03: " ++ (show test_rnd_fxn_03)
-  let parsedExpr = parse expr (show test_rnd_fxn_03)
-  putStrLn $ "parse expr test_rnd_fxn_03: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_int_rnd_fxn_01: " ++ (show test_int_rnd_fxn_01)
-  let parsedExpr = parse expr (show test_int_rnd_fxn_01)
-  putStrLn $ "parse expr test_int_rnd_fxn_01: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_int_rnd_fxn_02: " ++ (show test_int_rnd_fxn_02)
-  let parsedExpr = parse expr (show test_int_rnd_fxn_02)
-  putStrLn $ "parse expr test_int_rnd_fxn_02: " ++ (show parsedExpr)
-  putStrLn ""
-  putStrLn $ "test_int_rnd_fxn_03: " ++ (show test_int_rnd_fxn_03)
-  let parsedExpr = parse expr (show test_int_rnd_fxn_03)
-  putStrLn $ "parse expr test_int_rnd_fxn_03: " ++ (show parsedExpr)
-  putStrLn ""
-
-
-run_test = do                                                    
-  let arr = array ('A','Z') [(x,(NumConst 2)) | x <- ['A'..'Z']] 
-  let gen = ProgInfo (mkStdGen 10)                               
-  f <- (runReaderT $ ((runStateT (testing_fxn2 'A')) gen)) arr
-  return f
-
-
-testing_fxn2 :: Char -> StateT Program (ReaderT (Array Char Constant) IO) Float
-testing_fxn2 e = do
-  prog <- get
-  table <- lift ask
-  let r = num (table ! e)
-  let (value,newGen) = uniformR (0,r) (gen prog)
-  put (ProgInfo newGen)
-  return $ value
-
-
-testing_fxn :: Char -> ReaderT (Array Char Constant) (StateT Program IO) Float
-testing_fxn e = do
-  table <- ask
-  prog <- lift get
-  let r = num (table ! e)
-  let (value,newGen) = uniformR (0,r) (gen prog)
---  lift $ put $ ProgInfo newGen
-  return $ value
-
-
--- tests of statement parsing
-test_06 = do
-  putStrLn ""
-  putStrLn " TESTS OF STATEMENT PARSING "
-  putStrLn "============================"
-  putStrLn ""
-  let parsedExpr = parse statement "INPUT H"
-  putStrLn $ "parse statement \"INPUT H\": " ++ (show parsedExpr)
-  putStrLn ""
-  let parsedExpr = parse statement "LET X = INT(RND(5)*H+1)"
-  putStrLn $ "parse statement \"LET X = INT(RND(5)*H+1)\": " ++ 
-             (show parsedExpr)
-  putStrLn ""
-  let parsedExpr = parse statement "PRINT X"
-  putStrLn $ "parse statement \"PRINT X\": " ++ (show parsedExpr)
-  putStrLn ""
-  let parsedExpr = parse statement "FOR I = 1 TO H"
-  putStrLn $ "parse statement \"FOR I = 1 TO H\": " ++ (show parsedExpr)
-  putStrLn ""
-  let parsedExpr = parse statement "PRINT I"
-  putStrLn $ "parse statement \"PRINT I\": " ++ (show parsedExpr)
-  putStrLn ""
-  let parsedExpr = parse statement "IF I = X THEN 60"
-  putStrLn $ "parse statement \"IF I = X THEN 60\": " ++ (show parsedExpr)
-  putStrLn ""
-  let parsedExpr = parse statement "NEXT I"
-  putStrLn $ "parse statement \"NEXT I\": " ++ (show parsedExpr)
-  putStrLn ""
-  let parsedExpr = parse statement "END"
-  putStrLn $ "parse statement \"END\": " ++ (show parsedExpr)
-
-
